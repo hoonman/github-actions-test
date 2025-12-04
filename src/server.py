f1106ab9578bd72b8d7da03dc3b56900f992b409
hoonman <jh020812@gmail.com>
2025-12-03 18:43:42 -0800

    add server

---
+++ src/server.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"HELLO from Fly!\n")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    print("Server running on port 8080...")
+    server.serve_forever()
